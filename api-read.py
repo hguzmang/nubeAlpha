b80bab79f0876c29caa1a3b5478957866e0c7c75
hguzmang <hguzman.sistemas@gmail.com>
2025-02-20 10:43:00 -0600

    docker

---
+++ api-read.py
@@ -0,0 +1,7 @@
+#import requests
+#response  = requests.get(url)
+#if response.status_code == 200:
+#    data = response.json()
+#    print(data)
+#    services.swpc.noaa.gov  json goes 
+print ("hola")
